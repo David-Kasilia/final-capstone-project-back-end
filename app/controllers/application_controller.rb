47b0c3ab41623458fedc35fd3906ff51a9341026
David-Kasilia <davidkasilia001@gmail.com>
2023-02-09 01:41:47 +0300

    Updated roles in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::API
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-          user_params.permit(:name, :email, :password, :password_confirmation)
+          user_params.permit(:name, :email, :password, :password_confirmation, :role)
         end
     
         devise_parameter_sanitizer.permit(:sign_in) do |user_params|
