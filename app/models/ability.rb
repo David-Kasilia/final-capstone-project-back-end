9092784c8b484f899c341231057838b059a7a4f0
David-Kasilia <davidkasilia001@gmail.com>
2023-02-09 02:56:57 +0300

    Fix linters errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    
+
     case user.role
     when 'admin'
       can :manage, :all
@@ -18,6 +16,5 @@ class Ability
     else
       can :read, [Car]
     end
-  end
   end
 end
