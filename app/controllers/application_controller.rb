751fac8136e67dfb27a2144257fcf0b5e3049ecd
David-Kasilia <davidkasilia001@gmail.com>
2023-02-08 23:50:36 +0300

    Updated configured parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+          user_params.permit(:name, :email, :password, :password_confirmation)
+        end
+    
+        devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+          user_params.permit(:email, :password, :remember_me)
+        end
+
+        devise_parameter_sanitizer.permit(:account_update) do |user_params|
+            user_params.permit(:email, :password, :remember_me)
+        end
+    end
 end
